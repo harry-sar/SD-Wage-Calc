fcbd02abb6b8ff4f569ac094770b3574b5f16968
harry-sar <mail@harrysar.dev>
2021-11-03 00:07:18 +0000

    Initial Commit

---
+++ SD Wage Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SD Wage Calc
+//
+//  Created by Harry Sargeant on 03/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
